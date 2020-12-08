f68ae0aab9d96c7eca7cf323b6135febb26516aa
이상윤 <sangyun3631@gmail.com>
2020-12-08 17:35:28 +0900

    Add __init__ in tests

--- prothon/__init__.py
+++ prothon/__init__.py
@@ -1,6 +1,6 @@
 """Generate Proto files by Excel."""
-__version__ = '0.1.0'
-__all__ = 'generate',
+__version__ = '0.1.0',
+__all__ = ['generate', 'compile']
 
 from prothon.generator import generate
 from prothon.compiler import compile
