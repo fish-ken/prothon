d441a08263ee1f364a75943baef5f88190a603e2
이상윤 <sangyun3631@gmail.com>
2020-12-09 18:27:58 +0900

    Cleanup code

--- prothon/generator.py
+++ prothon/generator.py
@@ -8,51 +8,16 @@ def __is_excel_file(file_name):
         return False
     return True
 
-
-def __add_indent(syntax: str, indent_level: int):
-    indent = '\t' * indent_level
-    return indent + syntax
-
-
-def __make_field_syntax(option_name, type_name, field_name, field_index):
-    return '{} {} {} = {};\n'.format(option_name, type_name, field_name, field_index)
-
-
-def __find_row_index(sheet: openpyxl.worksheet.worksheet.Worksheet):
-    return 2
-
-
-def __generate_proto(sheet: openpyxl.worksheet.worksheet.Worksheet):
-
-    # Cache
-    indent_level = 0
-    file_name = sheet.name + '.proto'
-
-    contents = 'syntax = "proto3";\n\n'
-    contents += 'message {}\n{\n'.format(sheet.name)
-    indent_level += 1
-
-    row_index = __find_row_index(sheet)
-    for column_index in range(len(sheet[row_index])):
-        column_name = sheet[row_index][column_index].value
-
-        option_name = 'required'
-        type_name = column_name.split('[')[1:-1]
-        filed_name = column_name.split('[')[0]
-
-        __make_field_syntax()
-
-
-    f = open(file_name, 'w', encoding='utf8')
-    f.write(contents)
-    f.close()
-
-    print(sheet)
+# def __add_indent(syntax: str, indent_level: int):
+#     indent = '\t' * indent_level
+#     return indent + syntax
 
 
 def generate(excel_name):
     if __is_excel_file(excel_name) is False:
         pass
+
+    proto = 'syntax = "proto3";\n\n'
 
     workbook = openpyxl.load_workbook(excel_name)
 
@@ -60,8 +25,7 @@ def generate(excel_name):
         message = ProtoMessage(sheet)
 
         # TODO : Make hierarchy
-        
-        return message.make()
 
+        return proto + message.make()
 
     # TODO : Validate hierarchy
