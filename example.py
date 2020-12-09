91b1b6eb8bdf261c7210b21b9e2689e8ba7053ff
이상윤 <sangyun3631@gmail.com>
2020-12-09 18:21:54 +0900

    Fix name.proto bug

--- example.py
+++ example.py
@@ -1,5 +1,7 @@
 import prothon
+import os
 from glob import glob
+
 
 EXCEL_PATH = './tests/Excel/'
 EXPORT_PROTO_PATH = './Proto/'
@@ -19,7 +21,7 @@ def generate_proto(excel_path):
 if __name__ == '__main__':
     for excel_name in get_excel_list():
         proto = generate_proto(excel_name)
-        proto_name = excel_name.split('.')[0] + '.proto'
+        proto_name = os.path.basename(excel_name).split('.')[0] + '.proto'
 
         f = open(proto_name, 'w', encoding='utf8')
         f.write(proto)
