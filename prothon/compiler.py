af591ad1c104bfba95db1e6a38d5d2e6eda502fd
이상윤 <sangyun3631@gmail.com>
2020-12-11 18:20:03 +0900

    Add compiler option

--- prothon/compiler.py
+++ prothon/compiler.py
@@ -1,5 +1,15 @@
 
 
+compile_option_map = {
+    'csharp': 'csharp_out',
+    'cpp': 'cpp_output',
+    'python': 'python_out',
+    'java': 'java_out',
+    'go': 'go_out',
+    'dart': 'dart_out',
+}
+
+
 def __is_proto_file(file_name):
     if '$' in file_name or '.meta' in file_name:
         print('[ProtoGenerator] Not exceel file ' + file_name)
@@ -7,15 +17,14 @@ def __is_proto_file(file_name):
     return True
 
 # Compile *.proto
+
+
 def __compile_proto(sheet):
     print(sheet)
+
 
 def compile(proto_name):
     if __is_proto_file(proto_name) is False:
         pass
 
     __compile_proto(proto_name)
-    
-
-    
-
