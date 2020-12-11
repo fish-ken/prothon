2ab7f3ac85a4d196cae394f5bea0f9a4e6324bf0
이상윤 <sangyun3631@gmail.com>
2020-12-11 19:44:42 +0900

    Create proto_enum.py

---
+++ prothon/proto_enum.py
@@ -0,0 +1,50 @@
+from openpyxl.utils import get_column_letter
+from prothon.proto_base import ProtoBase
+
+
+ENUM_FORMAT = \
+    'enum {0}\n\
+{{\
+\n\
+{1}\
+\n\
+}}'
+
+
+
+class ProtoEnum(ProtoBase):
+    """
+    Proto enum declaration class
+
+    sheet: openpyxl.worksheet
+    column_header: column header like 'B'(second column)
+    name: enum type name
+    """
+
+    def __init__(self, sheet, column_header, name):
+        self.__sheet = sheet
+        self.__name = name
+        self.__column_header = column_header
+        self.__enum_elements = []
+        self.__initialize()
+
+    def __initialize(self):
+        columns = self.__sheet[self.__column_header]
+
+        for i in range(len(columns)):
+            value = columns[i].value
+            if value in self.__enum_elements:
+                continue
+            else:
+                self.__enum_elements.append(value)
+
+        pass
+
+    def make(self):
+        enum_fields = ''
+
+        for i in range(len(self.__enum_elements)):
+            field_index = i + 1
+            enum_fields += '\t{} = {};\n'.format(self.__enum_elements[i], field_index)
+
+        return ENUM_FORMAT.format(self.__name, enum_fields)
