f7f0dc8b06a8ed67025888025c4e6b330cd093f7
이상윤 <sangyun3631@gmail.com>
2020-12-14 17:09:28 +0900

    Update test_generate

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -1,22 +1,21 @@
-# import prothon
-# from glob import glob
+import os
+from glob import glob
 
-# EXCEL_PATH = './Excel/'
-# EXPORT_PROTO_PATH = './Proto/'
+EXCEL_PATH = './excel/'
+EXPORT_PROTO_PATH = './Proto/'
 
+def get_excel_list():
+    pattern = EXCEL_PATH + '[!~$]**[!*.meta]'
+    return glob(pattern)
 
+    
+# Return *.xlsx files name
+def test_generate():
+    for excel_path in get_excel_list():
+        name = os.path.basename(excel_name).split('.')[0]
+        cache_proto = open(EXPORT_PROTO_PATH + name, 'r', encoding='utf8').read()
+        gen_proto = prothon.generate(excel_path)
+        assert gen_proto == cache_proto
 
-# # Return *.xlsx files name
-# def get_excel_list():
-#     pattern = EXCEL_PATH + '[!~$]**[!*.meta]'
-#     return glob(pattern)
+    
 
-
-# # Generate *.proto by *.xlsx
-# def generate_proto(excel_path):
-#     prothon.generate(excel_path)
-
-
-# if __name__ == '__main__':
-#     for name in get_excel_list():
-#         generate_proto(name)
