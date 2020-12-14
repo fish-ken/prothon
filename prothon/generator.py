fcd053122b49f0d710dd74f2f4aa8bb6fa1cb4fb
이상윤 <sangyun3631@gmail.com>
2020-12-14 16:12:55 +0900

    Finally.... formatting...

--- prothon/generator.py
+++ prothon/generator.py
@@ -2,6 +2,7 @@ import openpyxl
 from prothon.proto_message import ProtoMessage
 from prothon.proto_const import ROOT_HIERARCHY_NAME
 
+
 def __is_excel_file(file_name):
     if '$' in file_name or '.meta' in file_name:
         print('[prothon] Not exceel file ' + file_name)
@@ -14,18 +15,30 @@ def __is_excel_file(file_name):
 
 
 def __format_proto(proto):
-    formatted = proto
-    formatted = formatted.replace('message ', '\nmessage ')
-    formatted = formatted.replace('{', '\n{\n')
-    formatted = formatted.replace('}', '}\n\n')
-    formatted = formatted.replace(';', ';\n')
-    formatted = formatted.replace('}\n\n}', '}\n}')
+    open_bracket = '{'
+    close_bracket = '}'
+    indent = '\t'
 
-    # for character in proto:
-    #     print(character)
+    formatted = ''
+    proto = proto.replace('message ', '\nmessage ')
+    proto = proto.replace(open_bracket, '\n{\n')
+    proto = proto.replace(close_bracket, '}\n\n')
+    proto = proto.replace(';', ';\n')
+    proto = proto.replace('}\n\n}', '}\n}')
+
+    indent_level = 0
+    for line in proto.splitlines():
+
+        if close_bracket in line:
+            indent_level -= 1
+
+        indents = indent * indent_level
+        formatted += indents + line + '\n'
+
+        if open_bracket in line:
+            indent_level += 1
 
     return formatted
-
 
 
 def generate(excel_name):
@@ -38,7 +51,7 @@ def generate(excel_name):
 
     message_map = {}
 
-    # Construct message   
+    # Construct message
     for sheet in workbook.worksheets:
         message = ProtoMessage(sheet)
         message_map[message.name] = message
@@ -51,10 +64,7 @@ def generate(excel_name):
             continue
 
         message_map[message.parent].add_message(message)
-        
+
     # Return proto string
 
-
     return proto + __format_proto(message_map[message_root_name].make())
-
-
